9b71693df14d47f03c28a0c7172128cb584df8d4
Aravindh <100423648+Aravindh187@users.noreply.github.com>
2022-08-16 18:58:11 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -7,6 +7,6 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-import pandas as pd
-my_fruit_list = pd.read_csv('fruit_macros.txt')
+import pandas
+my_fruit_list = pandas.read_csv('fruit_macros.txt')
 streamlit.DataFrame(my_fruit_list)
